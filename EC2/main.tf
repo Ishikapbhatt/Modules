70611167aed70fc01d96069d315a8df35730471d
Ishikapbhatt <ishikap1998@gmail.com>
2024-12-12 17:58:32 +0530

    Update main.tf

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,1 +1,9 @@
+resource "aws_instance" "main" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.public_subnet_id
 
+  tags = {
+    Name = var.ec2_name
+  }
+}
