60358ab7147d530cd19646d253097d914273c151
Ishikapbhatt <ishikap1998@gmail.com>
2024-12-12 18:00:38 +0530

    Update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,9 +1,55 @@
-resource "aws_instance" "main" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.public_subnet_id
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+root@ip-172-31-10-150:~/project/vpc# cat main.tf 
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = var.ec2_name
+    Name = var.vpc_name
   }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.vpc_name}-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr
+
+  tags = {
+    Name = "${var.vpc_name}-private-subnet"
+  }
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
 }
